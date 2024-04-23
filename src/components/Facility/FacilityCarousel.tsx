9690c8d71008d6dd14a67e49f5dce845a156112d
Ardianto Nugroho <ardi19nugroho@gmail.com>
2024-04-23 22:32:13 +0700

    feat: integrate facility carousel in home page

--- src/components/Facility/FacilityCarousel.tsx
+++ src/components/Facility/FacilityCarousel.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import Carousel from "../Carousel";
 import FacilityCarouselItem from "./FacilityCarouselItem";
+import { getFacilitiesAction } from "@/lib/actions/facilityAction";
+import Image from "next/image";
+import { ResponseFacility } from "@/types/facilityAction";
 
-const FacilityCarousel = () => {
+const FacilityCarousel = async () => {
+  const facilities = await getFacilitiesAction();
+  const currentFacilities = facilities?.data.slice(0, 3);
+
+  if (!facilities?.data.length) {
+    return (
+      <div className="w-full h-[350px] bg-myGreen1 grid place-items-center">
+        <Image
+          src={"/images/Hotel Booking-cuate.svg"}
+          typeof="image/svg"
+          width={300}
+          height={300}
+          alt="Hotel Booking-cuate"
+          className="size-[20rem]"
+          priority
+        />
+      </div>
+    );
+  }
+
   return (
     <Carousel>
-      <FacilityCarouselItem />
-      <FacilityCarouselItem />
-      <FacilityCarouselItem />
+      {currentFacilities.map((facility: ResponseFacility) => (
+        <FacilityCarouselItem key={facility.id} {...facility} />
+      ))}
     </Carousel>
   );
 };
--- src/components/Facility/FacilityCarouselItem.tsx
+++ src/components/Facility/FacilityCarouselItem.tsx
@@ -2,50 +2,58 @@ import React, { FunctionComponent } from "react";
 import { Users } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
+import { ResponseFacility } from "@/types/facilityAction";
+import { createTemplateSlug, dateFormatter } from "@/utils";
 
-const FacilityCarouselItem: FunctionComponent = () => {
+interface FacilityCarouselItemProps extends ResponseFacility {}
+
+const FacilityCarouselItem: FunctionComponent<FacilityCarouselItemProps> = (
+  props
+) => {
   return (
     <div className="relative bg-white">
       <Image
-        src={"/images/facility.jpg"}
-        alt="facility"
+        src={props.image}
+        alt={props.name}
         width={300}
         height={300}
-        className="h-full w-full animate-fade-in"
+        className="h-[350px] w-full animate-fade-in object-cover"
       />
       <div className="w-full h-full absolute z-[1]  hover:bg-gradient-to-t from-black/70 transition-all group/facility hover:backdrop-blur-sm top-0 flex items-end">
         <div className="p-4 hidden group-hover/facility:block">
           <Link
-            href={"/"}
-            className="text-white font-semibold text-2xl mb-2 block"
+            href={`/facility/${createTemplateSlug(props.name)}`}
+            className="text-white font-semibold text-2xl mb-2 block capitalize"
           >
-            Kolam Renang
+            {props.name}
           </Link>
           <div className="flex items-center gap-x-2 mb-5">
             <div
               className="rounded bg-white flex items-center gap-x-1 px-1.5 py-1"
               title="dibuat pada"
             >
-              <small className="text-xs">22 Feb 2023</small>
+              <small className="text-xs">
+                {dateFormatter(props.created_at.toString())}
+              </small>
             </div>
             <div
               className="rounded bg-primary text-white flex items-center gap-x-1 px-1.5 py-1"
               title="kapasitas"
             >
               <Users size={16} />
-              <small className="text-xs">3-10 orang</small>
+              <small className="text-xs">{props.capacities}</small>
             </div>
           </div>
           <div className="flex items-center gap-x-2">
             <Image
-              src={"/images/avatar/default.jpg"}
-              alt="default-avatar"
+              src={props.user?.image!}
+              alt={props.user?.name!}
               width={32}
               height={32}
               className="rounded-full w-8 h-8"
             />
             <div>
-              <p className="text-sm text-white">Ardianto</p>
+              <p className="text-sm text-white">{props.user?.name}</p>
               <p className="text-xs text-muted">(Admin)</p>
             </div>
           </div>
